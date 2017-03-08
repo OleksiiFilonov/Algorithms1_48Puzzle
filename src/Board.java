ce285a62451764b1912098da81055bd284b81464
Oleksii Filonov <oleksii.filonov@sephora.com>
2017-03-07 23:06:02 -0800

    Fix neighbors issue in Board. Replace to Stack in the Solver.solution() in order to iterate correctly from start to solution

--- src/Board.java
+++ src/Board.java
@@ -7,189 +7,193 @@ import edu.princeton.cs.algs4.StdRandom;
 
 public final class Board {
 
-	private final int[][] blocks;
-	private final int dimension;
-	private int emptyRow;
-	private int emptyColumn;
+    private final int[][] blocks;
+    private final int dimension;
+    private int emptyRow;
+    private int emptyColumn;
 
-	// construct a board from an n-by-n array of blocks
-	// (where blocks[i][j] = block in row i, column j)
-	public Board(int[][] inputBlocks) {
-		if (inputBlocks == null)
-			throw new NullPointerException("blocks can't be null");
-		this.dimension = inputBlocks.length;
-		this.blocks = new int[dimension][dimension];
-		for (int i = 0; i < dimension; i++) {
-			for (int j = 0; j < dimension; j++) {
-				this.blocks[i][j] = inputBlocks[i][j];
-				if (isEmptyBlock(i, j)) {
-					emptyRow = i;
-					emptyColumn = j;
-				}
-			}
-		}
-	}
+    // construct a board from an n-by-n array of blocks
+    // (where blocks[i][j] = block in row i, column j)
+    public Board(int[][] inputBlocks) {
+        if (inputBlocks == null)
+            throw new NullPointerException("blocks can't be null");
+        this.dimension = inputBlocks.length;
+        this.blocks = new int[dimension][dimension];
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                this.blocks[i][j] = inputBlocks[i][j];
+                if (isEmptyBlock(i, j)) {
+                    emptyRow = i;
+                    emptyColumn = j;
+                }
+            }
+        }
+    }
 
-	// board dimension n
-	public int dimension() {
-		return dimension;
-	}
+    // board dimension n
+    public int dimension() {
+        return dimension;
+    }
 
-	// number of blocks out of place
-	public int hamming() {
-		int priority = 0;
-		for (int i = 0; i < dimension; i++) {
-			for (int j = 0; j < dimension; j++) {
-				if (!isEmptyBlock(i, j) && !isBlockInPlace(i, j)) {
-					priority++;
-				}
-			}
-		}
-		return priority;
-	}
+    // number of blocks out of place
+    public int hamming() {
+        int priority = 0;
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                if (!isEmptyBlock(i, j) && !isBlockInPlace(i, j)) {
+                    priority++;
+                }
+            }
+        }
+        return priority;
+    }
 
-	private boolean isBlockInPlace(int i, int j) {
-		return blocks[i][j] == i * dimension + j + 1;
-	}
+    private boolean isBlockInPlace(int i, int j) {
+        return blocks[i][j] == i * dimension + j + 1;
+    }
 
-	// sum of Manhattan distances between blocks and goal
-	public int manhattan() {
-		int priority = 0;
-		for (int i = 0; i < dimension; i++) {
-			for (int j = 0; j < dimension; j++) {
-				if (!isEmptyBlock(i, j) && !isBlockInPlace(i, j)) {
-					int value = blocks[i][j];
-					int expectedRow = (value - 1) / dimension;
-					int expectedColumn = value % dimension - 1;
-					if (expectedColumn == -1)
-						expectedColumn = dimension - 1;
-					priority += abs(i - expectedRow) + abs(j - expectedColumn);
-				}
-			}
-		}
-		return priority;
-	}
+    // sum of Manhattan distances between blocks and goal
+    public int manhattan() {
+        int priority = 0;
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                if (!isEmptyBlock(i, j) && !isBlockInPlace(i, j)) {
+                    int value = blocks[i][j];
+                    int expectedRow = (value - 1) / dimension;
+                    int expectedColumn = value % dimension - 1;
+                    if (expectedColumn == -1)
+                        expectedColumn = dimension - 1;
+                    priority += abs(i - expectedRow) + abs(j - expectedColumn);
+                }
+            }
+        }
+        return priority;
+    }
 
-	private boolean isEmptyBlock(int i, int j) {
-		return blocks[i][j] == 0;
-	}
+    private boolean isEmptyBlock(int i, int j) {
+        return blocks[i][j] == 0;
+    }
 
-	private boolean isNotEmptyBlock(int[][] blocks, int i, int j) {
-		return blocks[i][j] != 0;
-	}
+    private boolean isNotEmptyBlock(int[][] blocks, int i, int j) {
+        return blocks[i][j] != 0;
+    }
 
-	// is this board the goal board?
-	public boolean isGoal() {
-		for (int i = 0; i < dimension; i++) {
-			for (int j = 0; j < dimension - 1; j++) {
-				if (!isBlockInPlace(i, j))
-					return false;
-			}
-		}
-		for (int j = 0; j < dimension - 1; j++) {
-			if (!isBlockInPlace(dimension - 1, j))
-				return false;
-		}
-		return true;
-	}
+    // is this board the goal board?
+    public boolean isGoal() {
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension - 1; j++) {
+                if (!isBlockInPlace(i, j))
+                    return false;
+            }
+        }
+        for (int j = 0; j < dimension - 1; j++) {
+            if (!isBlockInPlace(dimension - 1, j))
+                return false;
+        }
+        return true;
+    }
 
-	// a board that is obtained by exchanging any pair of blocks
-	public Board twin() {
-		Board twin = new Board(this.blocks);
-		boolean exchange = false;
-		while (!exchange) {
-			int i = StdRandom.uniform(dimension);
-			int j = StdRandom.uniform(dimension);
-			int ej = j;
-			if (j == dimension - 1) {
-				--ej;
-			} else {
-				++ej;
-			}
-			int[][] twinBlocks = twin.blocks;
-			if (isNotEmptyBlock(twinBlocks, i, j) && isNotEmptyBlock(twinBlocks, i, ej)) {
-				twin.swap(i, j, i, ej);
-				exchange = true;
-			}
-		}
-		return twin;
-	}
+    // a board that is obtained by exchanging any pair of blocks
+    public Board twin() {
+        Board twin = new Board(this.blocks);
+        boolean exchange = false;
+        while (!exchange) {
+            int i = StdRandom.uniform(dimension);
+            int j = StdRandom.uniform(dimension);
+            int ej = j;
+            if (j == dimension - 1) {
+                --ej;
+            } else {
+                ++ej;
+            }
+            int[][] twinBlocks = twin.blocks;
+            if (isNotEmptyBlock(twinBlocks, i, j) && isNotEmptyBlock(twinBlocks, i, ej)) {
+                twin.swap(i, j, i, ej);
+                exchange = true;
+            }
+        }
+        return twin;
+    }
 
-	private void swap(int i, int j, int ei, int ej) {
-		int temp = this.blocks[i][j];
-		this.blocks[i][j] = this.blocks[ei][ej];
-		this.blocks[ei][ej] = temp;
-	}
+    private void swap(int i, int j, int ei, int ej) {
+        int temp = this.blocks[i][j];
+        this.blocks[i][j] = this.blocks[ei][ej];
+        this.blocks[ei][ej] = temp;
+    }
 
-	@Override
-	// does this board equal y?
-	public boolean equals(Object other) {
-		if (other == this)
-			return true;
-		if (other == null)
-			return false;
-		if (other.getClass() != this.getClass())
-			return false;
-		Board that = (Board) other;
-		return Arrays.deepEquals(this.blocks, that.blocks);
-	}
+    @Override
+    // does this board equal y?
+    public boolean equals(Object other) {
+        if (other == this)
+            return true;
+        if (other == null)
+            return false;
+        if (other.getClass() != this.getClass())
+            return false;
+        Board that = (Board) other;
+        return Arrays.deepEquals(this.blocks, that.blocks);
+    }
 
-	// all neighboring boards
-	public Iterable<Board> neighbors() {
+    // all neighboring boards
+    public Iterable<Board> neighbors() {
 
-		Queue<Board> neighboards = new Queue<>();
-		// shift empty block to left
-		if (emptyColumn > 0) {
-			Board leftBoard = new Board(blocks);
-			leftBoard.swap(emptyRow, emptyColumn - 1, emptyRow, emptyColumn);
-			neighboards.enqueue(leftBoard);
-		}
-		// shift empty block to top
-		if (emptyRow > 0) {
-			Board topBoard = new Board(blocks);
-			topBoard.swap(emptyRow - 1, emptyColumn, emptyRow, emptyColumn);
-			neighboards.enqueue(topBoard);
-		}
-		// shift empty block to right
-		if (emptyColumn < dimension - 1) {
-			Board rightBoard = new Board(blocks);
-			rightBoard.swap(emptyRow, emptyColumn + 1, emptyRow, emptyColumn);
-			neighboards.enqueue(rightBoard);
-		}
-		// shift empty block to bottom
-		if (emptyRow < dimension - 1) {
-			Board bottomBoard = new Board(blocks);
-			bottomBoard.swap(emptyRow + 1, emptyColumn, emptyRow, emptyColumn);
-			neighboards.enqueue(bottomBoard);
-		}
-		return neighboards;
-	}
+        Queue<Board> neighboards = new Queue<>();
+        // shift empty block to left
+        if (emptyColumn > 0) {
+            final Board leftBoard = new Board(blocks);
+            leftBoard.swap(emptyRow, emptyColumn - 1, emptyRow, emptyColumn);
+            leftBoard.emptyColumn--;
+            neighboards.enqueue(leftBoard);
+        }
+        // shift empty block to top
+        if (emptyRow > 0) {
+            final Board topBoard = new Board(blocks);
+            topBoard.swap(emptyRow - 1, emptyColumn, emptyRow, emptyColumn);
+            topBoard.emptyRow--;
+            neighboards.enqueue(topBoard);
+        }
+        // shift empty block to right
+        if (emptyColumn < dimension - 1) {
+            final Board rightBoard = new Board(blocks);
+            rightBoard.swap(emptyRow, emptyColumn + 1, emptyRow, emptyColumn);
+            rightBoard.emptyColumn++;
+            neighboards.enqueue(rightBoard);
+        }
+        // shift empty block to bottom
+        if (emptyRow < dimension - 1) {
+            final Board bottomBoard = new Board(blocks);
+            bottomBoard.swap(emptyRow + 1, emptyColumn, emptyRow, emptyColumn);
+            bottomBoard.emptyRow++;
+            neighboards.enqueue(bottomBoard);
+        }
+        return neighboards;
+    }
 
-	@Override
-	// string representation of this board (in the output format specified
-	// below)
-	public String toString() {
-		StringBuilder s = new StringBuilder();
-		s.append(dimension + "\n");
-		for (int i = 0; i < dimension; i++) {
-			for (int j = 0; j < dimension; j++) {
-				s.append(String.format("%2d ", blocks[i][j]));
-			}
-			s.append("\n");
-		}
-		return s.toString();
-	}
+    @Override
+    // string representation of this board (in the output format specified
+    // below)
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(dimension + "\n");
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                s.append(String.format("%2d ", blocks[i][j]));
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
 
-	// unit tests (not graded)
-	public static void main(String[] args) {
-		int[][] test = new int[][] { { 1, 2 }, { 3, 4 }, { 15, 16 } };
-		int[][] testCopy = Arrays.copyOf(test, test.length);
-		for (int i = 0; i < testCopy.length; i++) {
-			for (int j = 0; j < testCopy[i].length; j++) {
-				System.out.printf("%3d", testCopy[i][j]);
-			}
-			System.out.println();
-		}
-		System.out.println(2 % 3);
-	}
+    // unit tests (not graded)
+    public static void main(String[] args) {
+        int[][] test = new int[][] { { 1, 2 }, { 3, 4 }, { 15, 16 } };
+        int[][] testCopy = Arrays.copyOf(test, test.length);
+        for (int i = 0; i < testCopy.length; i++) {
+            for (int j = 0; j < testCopy[i].length; j++) {
+                System.out.printf("%3d", testCopy[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println(2 % 3);
+    }
 }
--- src/Solver.java
+++ src/Solver.java
@@ -1,121 +1,125 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.MinPQ;
-import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdOut;
 
 public final class Solver {
 
-	private final SearchNode goalNode;
+    private final SearchNode goalNode;
+    private final boolean solvable;
+    private int numberSolutionMoves = -1;
 
-	private final class SearchNode implements Comparable<SearchNode> {
+    private final class SearchNode implements Comparable<SearchNode> {
 
-		private final SearchNode parentSearchNode;
-		private final int numberOfMoves;
-		private final Board board;
+        private final SearchNode parentSearchNode;
+        private final int numberOfMoves;
+        private final Board board;
+        private final int priority;
 
-		public SearchNode(SearchNode parentSearchNode, int numberOfMoves, Board board) {
-			this.parentSearchNode = parentSearchNode;
-			this.numberOfMoves = numberOfMoves;
-			this.board = board;
-		}
+        public SearchNode(SearchNode parentSearchNode, Board board) {
+            this.parentSearchNode = parentSearchNode;
+            if (parentSearchNode == null) {
+                this.numberOfMoves = 0;
+            } else {
+                this.numberOfMoves = parentSearchNode.numberOfMoves + 1;
+            }
+            this.board = board;
+            this.priority = board.manhattan() + numberOfMoves;
+        }
 
-		private int priority() {
-			return board.manhattan() + numberOfMoves;
-		}
+        @Override
+        public int compareTo(SearchNode otherSearchNode) {
+            if (this.priority > otherSearchNode.priority)
+                return 1;
+            if (this.priority < otherSearchNode.priority)
+                return -1;
+            return 0;
+        }
+    }
 
-		@Override
-		public int compareTo(SearchNode otherSearchNode) {
-			if (this.priority() > otherSearchNode.priority())
-				return 1;
-			if (this.priority() < otherSearchNode.priority())
-				return -1;
-			return 0;
-		}
-	}
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(final Board initial) {
+        if (initial == null)
+            throw new NullPointerException("Board can't be null");
+        final MinPQ<SearchNode> solutionPQ = new MinPQ<>();
+        final MinPQ<SearchNode> twinPQ = new MinPQ<>();
+        solutionPQ.insert(new SearchNode(null, initial));
+        twinPQ.insert(new SearchNode(null, initial.twin()));
+        SearchNode solutionNode = performSearchStep(solutionPQ);
+        SearchNode twinSolutionNode = performSearchStep(twinPQ);
+        while (!solutionNode.board.isGoal() && !twinSolutionNode.board.isGoal()) {
+            solutionNode = performSearchStep(solutionPQ);
+            twinSolutionNode = performSearchStep(twinPQ);
+        }
+        if (solutionNode.board.isGoal()) {
+            goalNode = solutionNode;
+            solvable = true;
+            numberSolutionMoves = goalNode.numberOfMoves;
+        } else {
+            goalNode = null;
+            solvable = false;
+        }
 
-	// find a solution to the initial board (using the A* algorithm)
-	public Solver(final Board initial) {
-		if (initial == null)
-			throw new NullPointerException("Board can't be null");
-		final MinPQ<SearchNode> solution = new MinPQ<>();
-		final MinPQ<SearchNode> twinSolution = new MinPQ<>();
-		solution.insert(new SearchNode(null, 0, initial));
-		twinSolution.insert(new SearchNode(null, 0, initial.twin()));
-		SearchNode solutionNode = performSearchStep(solution);
-		SearchNode twinSolutionNode = performSearchStep(twinSolution);
-		while (!solutionNode.board.isGoal() || !twinSolutionNode.board.isGoal()) {
-			solutionNode = performSearchStep(solution);
-			twinSolutionNode = performSearchStep(twinSolution);
-		}
-		if (solutionNode.board.isGoal()) {
-			goalNode = solutionNode;
-		} else {
-			goalNode = null;
-		}
+    }
 
-	}
+    private SearchNode performSearchStep(final MinPQ<SearchNode> searchQueue) {
+        final SearchNode currentSearchNode = searchQueue.delMin();
+        for (Board neighboard : currentSearchNode.board.neighbors()) {
+            if ((currentSearchNode.parentSearchNode == null)
+                    || !neighboard.equals(currentSearchNode.parentSearchNode.board)) {
+                searchQueue.insert(new SearchNode(currentSearchNode, neighboard));
+            }
+        }
+        return currentSearchNode;
+    }
 
-	private SearchNode performSearchStep(final MinPQ<SearchNode> searchQueue) {
-		final SearchNode parentNode = searchQueue.delMin();
-		for (Board neighboard : parentNode.board.neighbors()) {
-			if (!neighboard.equals(parentNode.board)) {
-				searchQueue.insert(new SearchNode(parentNode, parentNode.numberOfMoves + 1, neighboard));
-			}
-		}
-		return parentNode;
-	}
+    // is the initial board solvable?
+    public boolean isSolvable() {
+        return solvable;
+    }
 
-	// is the initial board solvable?
-	public boolean isSolvable() {
-		return goalNode != null;
-	}
+    // min number of moves to solve initial board; -1 if unsolvable
+    public int moves() {
+        return numberSolutionMoves;
+    }
 
-	// min number of moves to solve initial board; -1 if unsolvable
-	public int moves() {
-		if (isSolvable()) {
-			return goalNode.numberOfMoves;
-		} else {
-			return -1;
-		}
-	}
+    // sequence of boards in a shortest solution; null if unsolvable
+    public Iterable<Board> solution() {
+        if (isSolvable()) {
+            Stack<Board> solution = new Stack<>();
+            SearchNode currentNode = goalNode;
+            while (currentNode != null) {
+                solution.push(currentNode.board);
+                currentNode = currentNode.parentSearchNode;
+            }
+            return solution;
+        } else {
+            return null;
+        }
+    }
 
-	// sequence of boards in a shortest solution; null if unsolvable
-	public Iterable<Board> solution() {
-		if (isSolvable()) {
-			Queue<Board> solution = new Queue<>();
-			SearchNode currentNode = goalNode;
-			while (currentNode != null) {
-				solution.enqueue(currentNode.board);
-				currentNode = currentNode.parentSearchNode;
-			}
-			return solution;
-		} else {
-			return null;
-		}
-	}
+    // solve a slider puzzle (given below)
+    public static void main(String[] args) {
 
-	// solve a slider puzzle (given below)
-	public static void main(String[] args) {
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] blocks = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                blocks[i][j] = in.readInt();
+        Board initial = new Board(blocks);
 
-		// create initial board from file
-		In in = new In(args[0]);
-		int n = in.readInt();
-		int[][] blocks = new int[n][n];
-		for (int i = 0; i < n; i++)
-			for (int j = 0; j < n; j++)
-				blocks[i][j] = in.readInt();
-		Board initial = new Board(blocks);
+        // solve the puzzle
+        Solver solver = new Solver(initial);
 
-		// solve the puzzle
-		Solver solver = new Solver(initial);
-
-		// print solution to standard output
-		if (!solver.isSolvable())
-			StdOut.println("No solution possible");
-		else {
-			StdOut.println("Minimum number of moves = " + solver.moves());
-			for (Board board : solver.solution())
-				StdOut.println(board);
-		}
-	}
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                StdOut.println(board);
+        }
+    }
 }
